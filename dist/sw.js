dc98ab2d837cfbea15f37ff92bb869dbf9469876
LeTan1808 <lengoctan1808@gmail.com>
2026-03-04 14:20:04 +0700

    Add files via upload

--- dist/sw.js
+++ dist/sw.js
@@ -1,9 +1,9 @@
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : { title: 'Thông báo', body: 'Có cập nhật mới!' };
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: './icon.png'
-    })
-  );
+self.addEventListener('push', function (event) {
+    const data = event.data ? event.data.json() : { title: 'Thông báo', body: 'Có cập nhật mới!' };
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: './icon.png'
+        })
+    );
 });
